3d41de51de21d3c86bf9899295a2ac3954fb79ec
RymanAk <Rymanak@icloud.com>
2024-03-11 14:49:43 +0000

    made code pep8 compliant

--- src/find_disappeared_numbers/find_disappeared_numbers1.py
+++ src/find_disappeared_numbers/find_disappeared_numbers1.py
@@ -1,9 +1,11 @@
 from typing import List
+
+
 class Solution1:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         disappered_nums = []
         numbers = set(nums)
-        for i in range(1,len(nums)+1):
+        for i in range(1, len(nums) + 1):
             if i not in numbers:
                 disappered_nums.append(i)
         return disappered_nums
@@ -14,9 +16,14 @@ class Solution1:
 time complexity : O(n)
 space complexity : O(n)
 
-Time Complexity: O(n) - The dominant factor is the loop iterating through the range object (which depends on the length of the nums list). The creation of the set is also O(n).
+Time Complexity: O(n) - The dominant factor
+is the loop iterating through the range object
+(which depends on the length of the nums list).
+The creation of the set is also O(n).
 
-Space Complexity: O(n) - This comes from the creation of the set (numbers) and the disappered_nums list, both of which can potentially store n-1 elements in the worst case.
+Space Complexity: O(n) - This comes from the
+creation of the set (numbers) and the disappered_nums list,
+both of which can potentially store
+n-1 elements in the worst case.
 
 '''
-
--- tests/find_disappeared_numbers/test_find_disappeared_numbers.py
+++ tests/find_disappeared_numbers/test_find_disappeared_numbers.py
@@ -6,12 +6,13 @@ from src.find_disappeared_numbers.find_disappeared_numbers2 import Solution2
 s1 = Solution1()
 s2 = Solution2()
 
+
 @pytest.mark.parametrize("input_data, expected_output, description", [
     ([1], [], "Input list length 1"),
-    ([1,1], [2], "Input list length 2"),
-     ([2,2], [1], "Input list length 2"),
-    ([4,3,2,7,8,2,3,1], [5,6], "List length 8"),
-    (list(range(5,105)) + [5,6,7,8,9], [1,2,3,4,105], "Long list")
+    ([1, 1], [2], "Input list length 2"),
+    ([2, 2], [1], "Input list length 2"),
+    ([4, 3, 2, 7, 8, 2, 3, 1], [5, 6], "List length 8"),
+    (list(range(5, 105)) + [5, 6, 7, 8, 9], [1, 2, 3, 4, 105], "Long list")
 ])
 def test_find_disappeared_numbers(input_data, expected_output, description):
     assert s1.findDisappearedNumbers(
