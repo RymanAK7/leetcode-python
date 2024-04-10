e793763b708852226eec5eddeae4fbb53431507d
RymanAk <Rymanak@icloud.com>
2024-04-10 16:25:28 +0100

    Updated code and tests for find_if_path_exists to comply with PEP 8.

--- src/find_if_path_exists/find_if_path_exists.py
+++ src/find_if_path_exists/find_if_path_exists.py
@@ -1,18 +1,24 @@
 from typing import List
 
+
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    def validPath(
+            self,
+            n: int,
+            edges: List[List[int]],
+            source: int,
+            destination: int) -> bool:
         graph = self.create_graph(n, edges)
         return self.bfs(graph, source, destination)
-    
-    def create_graph(self, n: int,edges:  List[List[int]]) -> dict:
-        graph = {i:[] for i in range(0,n)}
+
+    def create_graph(self, n: int, edges: List[List[int]]) -> dict:
+        graph = {i: [] for i in range(0, n)}
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
         return graph
-    
-    def bfs(self,graph: dict, source: int, destination:int):
+
+    def bfs(self, graph: dict, source: int, destination: int):
         visited = set()
         queue = [source]
         visited.add(source)
@@ -25,18 +31,24 @@ class Solution:
                     visited.add(neigbour)
                     queue.append(neigbour)
         return False
-    
+
 
 '''
-
-time complexity is : O(m + n), where n is the number of nodes and m is the number of edges in the graph
+time complexity is : O(m + n),
+where n is the number of nodes
+and m is the number of edges in the graph
 
 Create Graph (create_graph method):
-The time complexity of creating the graph is O(m), where m is the number of edges.
+The time complexity of creating the graph
+is O(m), where m is the number of edges.
 
 BFS Traversal (bfs method):
-In the worst case, each node and edge are visited once, resulting in a time complexity of O(n + m).
-The loop may iterate through all nodes and edges in the worst case, but each node is visited at most once.
+In the worst case, each node and
+edge are visited once, resulting
+in a time complexity of O(n + m).
+The loop may iterate through all nodes
+and edges in the worst case, but
+each node is visited at most once.
 
 ----------------------------------------------------------
 
@@ -44,15 +56,25 @@ memory space : O(n + m)
 
 
 Create Graph (create_graph method):
-Since each node may have multiple neighbors, the space complexity for the graph representation is typically O(n + m), where n is the number of nodes and m is the number of edges.
+Since each node may have multiple neighbors,
+the space complexity for the graph
+representation is typically O(n + m),
+where n is the number of nodes and
+m is the number of edges.
 
 
 BFS Traversal (bfs method):
 
-The visited set in the bfs method is used to keep track of visited nodes during the BFS traversal. In the worst case, all nodes in the graph may need to be stored in the visited set, resulting in a space complexity of O(n).
+The visited set in the bfs method
+is used to keep track of visited nodes
+during the BFS traversal.
+In the worst case, all nodes in the graph
+may need to be stored in the visited set,
+resulting in a space complexity of O(n).
 
-The queue used for BFS traversal stores nodes to be visited. In the worst case, all nodes may need to be enqueued, resulting in a space complexity of O(n).
-
-
+The queue used for BFS traversal stores
+nodes to be visited. In the worst case,
+all nodes may need to be enqueued,
+resulting in a space complexity of O(n).
 
 '''
--- tests/find_if_path_exists/test_find_if_path_exists.py
+++ tests/find_if_path_exists/test_find_if_path_exists.py
@@ -3,9 +3,10 @@ from src.find_if_path_exists.find_if_path_exists import Solution
 
 s = Solution()
 
+
 def test_create_graph():
     '''returns a dict, keys are nodes from 0 to n-1
-       where n is the number of vertices. 
+       where n is the number of vertices.
        Values are lists containing the neighbors
        of the nodes which is the key'''
     expected_result = {
@@ -13,20 +14,22 @@ def test_create_graph():
         1: [0, 2],
         2: [0, 1]
     }
-    
+
     result = s.create_graph(3, [[0, 1], [1, 2], [2, 0]])
     for key in result:
         result[key].sort()
-    
+
     assert result == expected_result
-    
+
+
 def test_validPath_same_source_and_destination():
     # Test case where source and destination are the same
     n = 3
     edges = [[0, 1], [1, 2], [2, 0]]
     source = 0
     destination = 0
-    assert s.validPath(n, edges, source, destination) == True
+    assert s.validPath(n, edges, source, destination)
+
 
 def test_validPath_valid_path_exists():
     # Test case where a valid path exists from source to destination
@@ -34,7 +37,8 @@ def test_validPath_valid_path_exists():
     edges = [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]]
     source = 0
     destination = 5
-    assert s.validPath(n, edges, source, destination) == False
+    assert not s.validPath(n, edges, source, destination)
+
 
 def test_validPath_invalid_path():
     # Test case where no valid path exists from source to destination
@@ -42,6 +46,4 @@ def test_validPath_invalid_path():
     edges = [[1, 2], [3, 5], [5, 4], [4, 3]]
     source = 0
     destination = 2
-    assert s.validPath(n, edges, source, destination) == False    
-        
-    
+    assert not s.validPath(n, edges, source, destination)
